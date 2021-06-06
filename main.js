c92d12557767cd77018d31f8062bb4e725c91ee4
mosscodes <mossbusiness@outlook.com>
2021-06-06 18:11:32 -0400

    add html start button, add message styles, refactor js code, update winCheck, update changeTurn, add restartButton to generateBoard, add restartGame function, wrap code in game module for privacy

--- main.js
+++ main.js
@@ -1,133 +1,143 @@
-// this factory needs to be wrapped in a module to become private. wrap the whole game into a game module?
+const game = (() => {
+    // factory
+    const playerFactory = (marker, winnerValue) => {
+        return {marker, winnerValue}
+    }
 
-// factory
-const playerFactory = (marker, winnerValue) => {
-    return {marker, winnerValue}
-}
-
-const playerOne = playerFactory('X', 1)
-const playerTwo = playerFactory('O', -1)
+    const playerOne = playerFactory('X', 1)
+    const playerTwo = playerFactory('O', -1)
 
 
-// module
-const gameBoard = (() => {
+    // module
+    const gameBoard = (() => {
 
-    const gameBoardContainer = document.querySelector('#game-board-container');
+        const gameBoardContainer = document.querySelector('#game-board-container');
+        const startButtonContainer = document.querySelector("#start-button-container");
+        const startButton = document.querySelector("#start-button")
+        startButton.addEventListener('click', generateBoard);
 
-    function generateBoard() {
+        function generateBoard() {
+            for (let i = 0; i < 9; i++) {
+                const div = document.createElement('div')
+                div.setAttribute('class', 'grid-div')
+                div.setAttribute('id', `grid-div-${i + 1}`)
+                div.innerText = "";
+                div.addEventListener('click', function() {gameflow.updateBoard(i+1);});
+                gameBoardContainer.appendChild(div);
+            }
+            
+            // deletes start button and makes a restart button in it's place
+            document.querySelector("#message-container").innerText = "X Turn"
+            startButton.remove()
+            const restartButton = document.createElement('button');
+            restartButton.setAttribute('id', 'start-button');
+            restartButton.addEventListener('click', gameflow.restartGame)
+            restartButton.innerText = "Restart Game"
+            startButtonContainer.appendChild(restartButton);
+        }
+    })();
+
+    // module
+    const gameflow = (() => {
+
+        let boardObj = {}
         for (let i = 0; i < 9; i++) {
-            const div = document.createElement('div')
-            div.setAttribute('class', 'grid-div')
-            div.setAttribute('id', `grid-div-${i + 1}`)
-            div.innerText = "";
-            div.addEventListener('click', function() {gameflow.updateBoard(i+1);});
-            gameBoardContainer.appendChild(div);
+            boardObj[`${i+1}`] = 0;
         }
-        const buttonContainer = document.querySelector('#start-button-container')
-        const startButton = document.createElement('button')
-        startButton.setAttribute('id', 'start-button')
-        startButton.innerText = "Start Game!";
-        buttonContainer.appendChild(startButton)
-    }
 
-    return {
-        generateBoard,
-    };
-})();
+        let turnCount = 0;
+        const messageContainer = document.querySelector("#message-container");
+        let isWinner = false;
 
-// module
-const gameflow = (() => {
-
-    let boardObj = {}
-    for (let i = 0; i < 9; i++)
-        boardObj[`${i+1}`] = 0;
-
-    function gameStart() {
-
-        // add html button
-        // add event listeners to all grid divs
-        // instead of adding line 23 at generate board, add updateBoard functionality on gamestart
-        // div.addEventListener('click', function() {gameflow.updateBoard(i+1);});
-
-        // currentTurn = playerOne
-        // change start button to say 'restart'
-        // if button.innertext = 'restart, clear board, boardObj, currentTurn = playerOne, turnCount = 0
-
-
-    }
-
-    function updateBoard(index) {
-        if (boardObj[index] === 0) {
-            boardObj[index] = currentTurn.winnerValue;
-            document.querySelector(`#grid-div-${index}`).innerText = currentTurn.marker;
-            changeTurn()
-            turnCount++
-        }
-    }
-
-    let turnCount = 0
-
-    // winning combinations: 123, 456, 789, 147, 258, 369, 159, 753
-    // minimum placements until winCheck needs to be executed: 5 placements minimum to win
-    // 9 placements to draw
-
-    // add functionality to remove the ability to updateboard
-    function winCheck(boardObj) {
-        if (turnCount >= 5) {
-            // check rows
-            for (let i = 1; i <= 9; i = i + 3) {
-                if (boardObj[i] + boardObj[i+1] + boardObj[i+2] === 3) {
-                    alert("X Wins!")
-                }
-                else if (boardObj[i] + boardObj[i+1] + boardObj[i+2] === -3) {
-                    alert("O Wins!")
-                }
-            }
-            // check columns
-            for (let i =1; i <= 3; i++) {
-                if (boardObj[i] + boardObj[i+3] + boardObj[i+6] === 3) {
-                    alert("X Wins!")
-                }
-                else if (boardObj[i] + boardObj[i+3] + boardObj[i+6] === -3) {
-                    alert("O Wins!")
-                }
-            }
-            // check diagonals
-            if (boardObj[1] + boardObj[5] + boardObj[9] === 3) {
-                alert("X Wins!")
-            }
-            else if (boardObj[1] + boardObj[5] + boardObj[9] === -3) {
-                alert("O Wins!")
-            }
-            else if (boardObj[3] + boardObj[5] + boardObj[7] === 3) {
-                alert("X Wins!")
-            }
-            else if (boardObj[3] + boardObj[5] + boardObj[7] === -3) {
-                alert("0 Wins!")
-            }
-            // check draw
-            else if (turnCount === 9) {
-                alert("It's a Draw!")
-            }
-        }
-    }
-
-    let currentTurn = playerOne
-    function changeTurn() {
-        if (currentTurn === playerOne) {
-            currentTurn = playerTwo;
-        }
-        else {
+        function restartGame() {
+            isWinner = false;
             currentTurn = playerOne;
+            turnCount = 0;
+            boardObj = {};
+            for (let i = 0; i < 9; i++) {
+                boardObj[`${i+1}`] = 0;
+                document.querySelector(`#grid-div-${i+1}`).innerText = "";
+            }
+            messageContainer.innerText = "X Turn";
         }
-    }
 
-    return {
-        updateBoard,
-        winCheck,
-        boardObj
-    }
+        function updateBoard(index) {
+            if (isWinner === false) {
+                if (boardObj[index] === 0) {
+                    boardObj[index] = currentTurn.winnerValue;
+                    document.querySelector(`#grid-div-${index}`).innerText = currentTurn.marker;
+                    changeTurn()
+                    turnCount++
+                    winCheck(boardObj)
+                }
+            }
+        }
+
+        function winCheck(boardObj) {
+            if (turnCount >= 5) {
+
+                // check rows
+                for (let i = 1; i <= 9; i = i + 3) {
+                    if (boardObj[i] + boardObj[i+1] + boardObj[i+2] === 3) {
+                        messageContainer.innerText = "X Wins!";
+                        isWinner = true;
+                    }
+                    else if (boardObj[i] + boardObj[i+1] + boardObj[i+2] === -3) {
+                        messageContainer.innerText = "O Wins!";
+                        isWinner = true;
+                    }
+                }
+                // check columns
+                for (let i =1; i <= 3; i++) {
+                    if (boardObj[i] + boardObj[i+3] + boardObj[i+6] === 3) {
+                        messageContainer.innerText = "X Wins!";
+                        isWinner = true;
+                    }
+                    else if (boardObj[i] + boardObj[i+3] + boardObj[i+6] === -3) {
+                        messageContainer.innerText = "O Wins!";
+                        isWinner = true;
+                    }
+                }
+                // check diagonals
+                if (boardObj[1] + boardObj[5] + boardObj[9] === 3) {
+                    messageContainer.innerText = "X Wins!";
+                    isWinner = true;
+                }
+                else if (boardObj[1] + boardObj[5] + boardObj[9] === -3) {
+                    messageContainer.innerText = "O Wins!";
+                    isWinner = true;
+                }
+                else if (boardObj[3] + boardObj[5] + boardObj[7] === 3) {
+                    messageContainer.innerText = "X Wins!";
+                    isWinner = true;
+                }
+                else if (boardObj[3] + boardObj[5] + boardObj[7] === -3) {
+                    messageContainer.innerText = "O Wins!";
+                    isWinner = true;
+                }
+                // check draw
+                else if (turnCount === 9) {
+                    messageContainer.innerText = "It's a Draw!";
+                    isWinner = true;
+                }
+            }
+        }
+
+        let currentTurn = playerOne
+        function changeTurn() {
+            if (currentTurn === playerOne) {
+                currentTurn = playerTwo;
+                messageContainer.innerText = "O Turn"
+            }
+            else {
+                currentTurn = playerOne;
+                messageContainer.innerText = "X Turn"
+            }
+        }
+
+        return {
+            updateBoard,
+            restartGame
+        }
+    })();
 })();
-
-
-gameBoard.generateBoard();
